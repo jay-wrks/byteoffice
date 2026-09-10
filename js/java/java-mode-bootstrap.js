d504e5c944c5997b11bf84146f9e9f8fe10bd7c6
Jay <jay.code.works@gmail.com>
2026-09-11 03:03:27 +0530

    fix(ide): make execution shimmer run once

--- js/java/java-mode-bootstrap.js
+++ js/java/java-mode-bootstrap.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  const ASSET_VERSION='20260911-0082';
+  const ASSET_VERSION='20260911-0083';
 
   function versioned(src){
     return src + (src.includes('?')?'&':'?') + 'v=' + encodeURIComponent(ASSET_VERSION);
