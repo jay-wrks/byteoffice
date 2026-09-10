c9132284b6497a439ad8d89c3b2a1f7b980ecbab
Jay <jay.code.works@gmail.com>
2026-09-11 01:02:21 +0530

    fix(ide): synchronize Java asset cache version

--- js/java/java-mode-bootstrap.js
+++ js/java/java-mode-bootstrap.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  const ASSET_VERSION='20260911-0048';
+  const ASSET_VERSION='20260911-0055';
 
   function versioned(src){
     return src + (src.includes('?')?'&':'?') + 'v=' + encodeURIComponent(ASSET_VERSION);
