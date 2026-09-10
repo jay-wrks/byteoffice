cf12305dc907efceb4ca6a049d358f4dfdbad852
Jay <jay.code.works@gmail.com>
2026-09-11 03:58:24 +0530

    fix(java): show ByteBot API tray methods

--- js/java/java-mode-bootstrap.js
+++ js/java/java-mode-bootstrap.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  const ASSET_VERSION='20260911-0103';
+  const ASSET_VERSION='20260911-0104';
 
   function versioned(src){
     return src + (src.includes('?')?'&':'?') + 'v=' + encodeURIComponent(ASSET_VERSION);
