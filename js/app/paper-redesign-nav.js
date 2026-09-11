7af3c0772ce75317c856942212d4bd7d3c9ddb48
jay-wrks <jay.code.works@gmail.com>
2026-09-12 00:06:03 +0530

    Wire redesigned home navigation

--- js/app/paper-redesign-nav.js
+++ js/app/paper-redesign-nav.js
@@ -3,13 +3,14 @@
     const el = document.getElementById(id);
     if (el) el.addEventListener('click', fn);
   };
-
-  on('homeLevelsNav', () => openRoadmap('home'));
-  on('homeDashboardNav', () => showDashboard());
-  on('homeBadgesNav', () => showAchievements());
-  on('homeHelpNav', () => showHelp());
-  on('homeSettingsNav', () => {
+  const openSettings = () => {
     const settingsButton = document.getElementById('homeSettingsBtn');
     if (settingsButton) settingsButton.click();
-  });
+  };
+
+  ['homeLevelsBtn','homeTopLevelsBtn'].forEach(id=>on(id,()=>openRoadmap('home')));
+  ['homeDashboardBtn','homeTopDashboardBtn'].forEach(id=>on(id,()=>showDashboard()));
+  ['homeBadgesBtn','homeTopBadgesBtn'].forEach(id=>on(id,()=>showAchievements()));
+  ['homeHelpBtn','homeTopHelpBtn'].forEach(id=>on(id,()=>showHelp()));
+  ['homeSideSettingsBtn','homeTopSettingsBtn'].forEach(id=>on(id,openSettings));
 })();
